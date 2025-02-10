eb4921ed89892acb6ffb9060421d075d9ad587f1
Aboubacar Sidig DIALLO <Aboubacar.sidig@gmail.com>
2025-02-10 16:18:14 +0100

    Affichage produit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { LoggingInterceptor } from './midllewares/logging.interceptor';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalInterceptors(new LoggingInterceptor()); // Enregistrer l'interceptor globalement
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3001);
 }
 bootstrap();
