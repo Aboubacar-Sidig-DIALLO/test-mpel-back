e88230c21e6f852908e79327a7bdde2ad5e46967
Aboubacar Sidig DIALLO <Aboubacar.sidig@gmail.com>
2025-02-10 17:34:03 +0100

    Fix bug

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { ProductController } from './controllers/product/product.controller';
+import { ProductService } from './services/product/product.service';
+import { PrismaService } from './services/prisma/prisma.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, ProductController],
+  providers: [AppService, ProductService, PrismaService],
 })
 export class AppModule {}
