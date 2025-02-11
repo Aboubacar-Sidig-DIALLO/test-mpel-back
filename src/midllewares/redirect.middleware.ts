e9f4d4e5b7daf627c18d63015780ab7f4bcbc054
Aboubacar Sidig DIALLO <Aboubacar.sidig@gmail.com>
2025-02-11 08:56:53 +0100

    Fix bug connexion db

--- src/midllewares/redirect.middleware.ts
+++ src/midllewares/redirect.middleware.ts
@@ -4,7 +4,6 @@ import { Request, Response, NextFunction } from 'express';
 @Injectable()
 export class RedirectMiddleware implements NestMiddleware {
     use(req: Request, res: Response, next: NextFunction) {
-      console.log("LURL => ", req.url === '/')
         if (req.url === '/') {
             res.redirect(301, '/product?page=1'); // Redirection permanente
         } else {
