9f56891e9b85e1da57528efe61e019c503a009b0
Aboubacar Sidig DIALLO <Aboubacar.sidig@gmail.com>
2025-02-11 10:02:12 +0100

    Gestion des autorisations

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,4 @@
 import { MiddlewareConsumer, Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ProductController } from './controllers/product/product.controller';
 import { ProductService } from './services/product/product.service';
 import { PrismaService } from './services/prisma/prisma.service';
@@ -8,8 +6,8 @@ import { RedirectMiddleware } from './midllewares/redirect.middleware';
 
 @Module({
   imports: [],
-  controllers: [AppController, ProductController],
-  providers: [AppService, ProductService, PrismaService],
+  controllers: [ProductController],
+  providers: [ProductService, PrismaService],
 })
 export class AppModule {
   configure(consumer: MiddlewareConsumer) {
